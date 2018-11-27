f4d43dbebce74129e5b6658c02ae8f3ddd0d05b1
Camille <camilleclaret@orange.fr>
2018-11-27 14:52:17 +0100

    First Commit, début du controleur

--- src/main/java/Controler/Controler.java
+++ src/main/java/Controler/Controler.java
@@ -2,11 +2,13 @@ package Controler;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet(name = "Controler", urlPatterns = {"/Controler"})
@@ -21,12 +23,73 @@ public class Controler extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException 
+    {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            
+        if (actionIsClient(request, "Connexion")) 
+        {
+            // Ici on lance alors la connexion et la page adequate
+        } else if (actionIsAdmin(request, "Connexion"))
+        {
+            // Ici on lance alors la connexion admin er la page adequate
         }
+       
+    }
+    /**
+     * Fonction qui retourne un boolean de la validité de l'action du client
+     * @param request
+     * @param action
+     * @return 
+     */
+    private boolean actionIsClient(HttpServletRequest request, String action) 
+    {
+        return action.equals(request.getParameter("actionClient"));
+    }
+    
+    /**
+     * Fonction qui retourne un boolean de la validité de l'action de l'admin
+     * @param request
+     * @param action
+     * @return 
+     */
+    private boolean actionIsAdmin(HttpServletRequest request, String action) 
+    {
+        return action.equals(request.getParameter("actionAdmin"));
+    }
+    
+    /**
+     * Fonction permettant d'afficher une vue
+     * @param jsp
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    private void showView(String jsp, HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException 
+    {
+        getServletConfig().getServletContext().getRequestDispatcher("/views/" + jsp).forward(request, response);
+    }
+    
+    /**
+     * Fonction de permettant la connextion
+     * @param request 
+     * @param response 
+     */
+    private void newConnect(HttpServletRequest request, HttpServletResponse response, boolean isAd ) 
+            throws ServletException, IOException
+    {
+        HttpSession session = request.getSession();
+        String login = (String) session.getAttribute("login");
+        String pwd = (String) session.getAttribute("mdp");
+        if (isAd)
+        {
+            showView("AdminPage.jsp", request, response);
+        } else {
+            showView("ClientPagge.jsp", request, response);
+        }
+        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
